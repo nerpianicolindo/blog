8fae2dc03bf4b0db5d4360b7ffb88ec4f26bac03
Nerpi <algoal1994@gmail.com>
2020-01-16 18:06:32 +0100

    Crear scope en post

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -9,10 +9,7 @@ class PagesController extends Controller
 {
     public function home()
     {
-        $posts = Post::whereNotNull('published_at')
-                    ->where('published_at', '<=', Carbon::now())
-                    ->latest('published_at')
-                    ->get();
+        $posts = Post::published()->get();
         return view('welcome', compact('posts'));
     }
 }
